b3127529077d0b1dca20601b4dcdf41de578f8a1
Swapnil Barwat <sbarwat@bridge2solutions.com>
2017-06-16 20:28:23 -0400

    - Added single client for all services with workload in json.

--- client/client/client.py
+++ client/client/client.py
@@ -12,10 +12,16 @@ from tornado.httpclient import AsyncHTTPClient
 
 import time
 
+import MySQLdb
+
 DEMO_APP_HOST = os.getenv('DEMO_APP_HOST', 'localhost')
 DEMO_APP_PORT = os.getenv('DEMO_APP_PORT', '9000')
 DEMO_CONFIG_FILE = os.getenv('DEMO_CONFIG_FILE', 'requests.json')
 DEMO_APP_URL = "http://" + DEMO_APP_HOST + ":" + DEMO_APP_PORT + "/callhttp"
+MYSQL_HOST = os.getenv('MYSQL_HOST', '127.0.0.1')
+MYSQL_USER = os.getenv('MYSQL_USER', 'root')
+MYSQL_PWD = os.getenv('MYSQL_PWD', '')
+REDIS_HOST = os.getenv('REDIS_HOST', '172.17.0.2')
 
 
 def async_client():
@@ -30,7 +36,7 @@ def async_client():
         data=json.loads(f.read())
         f.close()
 
-        for request in data['requests']:
+        for request in data['http']:
             success_count=int(request['success']['count'])
             sendSuccessRequests(http_client, success_count)
             errorList=request['errors']
@@ -93,7 +99,64 @@ class Request():
     def __init__(self,response_code):
         self.response_code=response_code
 
+def connectMysqlDB():
+    db = MySQLdb.connect(host=MYSQL_HOST,    # your host, usually localhost
+                         user=MYSQL_USER,         # your username
+                         passwd=MYSQL_PWD)  # your password
+
+    cur = db.cursor()
+
+    cur.execute("CREATE DATABASE IF NOT EXISTS testdb")
+    cur.execute("use testdb")
+    cur.execute("DROP TABLE IF EXISTS employee")
+    cur.execute("CREATE TABLE employee (id INT(6) NOT NULL AUTO_INCREMENT, fname VARCHAR(30), lname VARCHAR(30), PRIMARY KEY (id))")
+
+    with open(DEMO_CONFIG_FILE) as f:
+        data=json.loads(f.read())
+        f.close()
+
+        for command in data['mysql']:
+            command=data['mysql']['command']
+            count=data['mysql']['count']
+            for i in range(int(count)):
+                #if query is insert and already not run
+                if(command.find("insert") && !isInsertdone):
+                    cur.execute("Select count(*) from employee")
+                    result=cur.fetchone()
+                    #no of records does not match with count in json then keep on inserting.
+                    if(result == int(count)):
+                        isInsertdone=true
+                    else:
+                        cur.execute(command)
+                        for row in cur:
+                            print(row)
+                else:
+                    cur.execute(command)
+                        for row in cur:
+                            print(row)
+    db.close()
+
+def redisClient():
+    r = redis.StrictRedis(host=REDIS_HOST, port=6379, db=0)
+
+    i=0
+    while(1):
+        print("Adding key redis-key-" + str(i))
+        r.set('redis-key-' + str(i), 'redis-value-' + str(i))
+        time.sleep(5)
+        print("Reading redis-key-" + str(i) + " with value " + r.get('key-'+str(i)))
+        print ("waiting for 5 sec..")
+        #deleting key once insert and read is finished
+        r.delete('redis-key-'+str(i))
+        i=i+1
+
+global isInsertdone= false
 while(1):
+    print("Calling http client")
     async_client()
-    time.sleep(5)
+    print("Calling mysql client")
+    connectMysqlDB()
+    print("calling redis client")
+    redisClient()
     print("Waiting for 5 sec...")
+    time.sleep(5)
