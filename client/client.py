ddfd79d883354c45670e174198acb69dfebd4d3f
Swapnil Barwat <sbarwat@bridge2solutions.com>
2017-06-23 00:21:48 -0400

    - Added table check in dynamodb

--- client/client.py
+++ client/client.py
@@ -235,41 +235,51 @@ def dyanamoDB():
     dynamoDBReadItem(dynamodb,recordCount)
 
 def dynamoDBCreateTable(dClient):
+    isTableExist=False
+    try:
+        response = dClient.describe_table(
+            TableName='users'
+        )
+    except ResourceNotFoundException as e:
+            print (str(e))
+            isTableExist=True
+    if(isTableExist == False):
     # Create the DynamoDB table.
-    table = dClient.create_table(
-        TableName='users',
-        KeySchema=[
-            {
-                'AttributeName': 'username',
-                'KeyType': 'HASH'
-            },
-            {
-                'AttributeName': 'last_name',
-                'KeyType': 'RANGE'
+        table = dClient.create_table(
+            TableName='users',
+            KeySchema=[
+                {
+                    'AttributeName': 'username',
+                    'KeyType': 'HASH'
+                },
+                {
+                    'AttributeName': 'last_name',
+                    'KeyType': 'RANGE'
+                }
+            ],
+            AttributeDefinitions=[
+                {
+                    'AttributeName': 'username',
+                    'AttributeType': 'S'
+                },
+                {
+                    'AttributeName': 'last_name',
+                    'AttributeType': 'S'
+                },
+
+            ],
+            ProvisionedThroughput={
+                'ReadCapacityUnits': 5,
+                'WriteCapacityUnits': 5
             }
-        ],
-        AttributeDefinitions=[
-            {
-                'AttributeName': 'username',
-                'AttributeType': 'S'
-            },
-            {
-                'AttributeName': 'last_name',
-                'AttributeType': 'S'
-            },
+        )
+        # Wait until the table exists.
+        table.meta.client.get_waiter('table_exists').wait(TableName='users')
 
-        ],
-        ProvisionedThroughput={
-            'ReadCapacityUnits': 5,
-            'WriteCapacityUnits': 5
-        }
-    )
-
-    # Wait until the table exists.
-    table.meta.client.get_waiter('table_exists').wait(TableName='users')
-
-    # Print out some data about the table.
-    print(table.item_count)
+        # Print out some data about the table.
+        print(table.item_count)
+    else:
+        print("Table already present. Skipping..")
 
 def dynamoDBCreateItem(dClient, count):
     table = dClient.Table('users')
