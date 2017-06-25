eb5925168794284871eaa89b17551f5c8a3c91b9
Swapnil Barwat <sbarwat@bridge2solutions.com>
2017-06-25 15:44:55 -0400

    - python script to update configmap

--- client/updateconfigmap.py
+++ client/updateconfigmap.py
@@ -13,8 +13,12 @@ deployFile['spec']['replicas'] = 0
 with  open(filename,"w") as f:
 	yaml.dump(deployFile, f)
 
+print("Scaling down to 0")
 os.system("kubectl apply -f " + filename)
 
 deployFile['spec']['replicas'] = originalReplicaCount
 with  open(filename,"w") as f:
 	yaml.dump(deployFile, f)
+
+print("Scaling down to original value" + originalReplicaCount)
+os.system("kubectl apply -f " + filename)
