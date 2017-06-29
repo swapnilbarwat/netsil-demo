ad46ef62accb32b483d3babc6b59f7d93c30e0f1
Swapnil Barwat <sbarwat@bridge2solutions.com>
2017-06-28 21:38:04 -0400

    print data

--- client/client.py
+++ client/client.py
@@ -328,18 +328,11 @@ def dynamoDBReadItem(isAWS,count, region,accessKeyId,secretKeyId):
          # Get the service resource.
         dClient = boto3.resource('dynamodb',endpoint_url=DYNAMODB_HOST_URL,region_name=region,aws_access_key_id=accessKeyId,aws_secret_access_key=secretKeyId)
     table = dClient.Table('users')
-    # for i in range(count):
-    #     response = table.get_item(
-    #         Key={
-    #             'username': str('janedoe'+str(i)),
-    #             'last_name': str('doe'+str(i))
-    #             }
-    #     )
-        # item = response['Item']
-        # print(item)
-    data=table.scan()
-    for i in data:
-        print i
+
+    data=table.scan(ConsistentRead=True)
+    items = all_admins['Items']
+    for x in items:
+        print "dynamodb-"  + str(x['username'])
 
 def postgres():
     try:
